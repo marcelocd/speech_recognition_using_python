a72faba12f1e25915c729803fa1abce97e26426f
[marcelocd] <[marcelocdias93@gmail.com]>
2020-06-25 19:55:11 -0300

    YES! IT WORKS NOW! THANK YOU HIMANSHU SHARMA FROM STACKOVERFLOW! HAHAHAHA

--- speech.py
+++ speech.py
@@ -3,11 +3,15 @@ import speech_recognition as sr
 r = sr.Recognizer()
 
 with sr.Microphone() as source:
+	r.adjust_for_ambient_noise(source)
+
 	print('Say anything: ')
+
 	audio = r.listen(source)
 
 	try: 
 		text = r.recognize_google(audio)
+		
 		print('You said: {}'.format(text))
 	except:
 		print('Sorry. Could not recognize your voice')
